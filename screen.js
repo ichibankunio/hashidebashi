cfd3f4c32787c69b7cb4de9099c6a53b355d6238
ichibankunio <89119966+ichibankunio@users.noreply.github.com>
2021-11-28 18:43:17 +0900

    Add files via upload

---
+++ screen.js
@@ -0,0 +1,46 @@
+// canvas = document.Call("createElement", "canvas")
+// canvas.Set("width", 16)
+// canvas.Set("height", 16)
+
+// document.Get("body").Call("appendChild", canvas)
+// var canvas = document.createElement("canvas");
+// var canvas = document.body.firstElementChild;
+// console.log(canvas.clientWidth, canvas.clientHeight);
+
+// document.body.onclick = function(e) {
+//     var canvas = document.body.firstElementChild;
+//     console.log(canvas.clientWidth, canvas.clientHeight);
+// };
+
+// if (!alert('OKを押してください。')) {
+//     // OKが押された際に実行する処理
+//     document.addEventListener("DOMContentLoaded", function(){
+//         var canvas = document.body.firstElementChild;
+//         console.log(canvas.clientWidth, canvas.clientHeight)
+    
+//         canvas.addEventListener('onclick',function(){ //addEventListenerとはなんなのか
+//             console.log(canvas.clientWidth, canvas.clientHeight);
+//         })
+//     }, false);
+// }
+
+// window.onload = function() {
+//     document.body.addEventListener("click", function(event){
+//         var touch = event.changedTouches[0];
+    
+//         var x = touch.pageX;
+//         var y = touch.pageY;
+//         console.log("Hello", x, y)
+//         // if (x)
+//         // var canvas = document.body.firstElementChild;
+//         // console.log(canvas.clientWidth, canvas.clientHeight)
+    
+//         // canvas.addEventListener('onclick',function(){ //addEventListenerとはなんなのか
+//         //     console.log(canvas.clientWidth, canvas.clientHeight);
+//         // })
+//     }, false);
+// }
+
+
+
+
